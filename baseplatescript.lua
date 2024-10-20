83326667e09b5e2f042f3a8c82a64a28a8532418
pacjaxy <154277055+pacjaxy@users.noreply.github.com>
2024-10-20 13:20:50 -0700

    Add files via upload

---
+++ baseplatescript.lua
@@ -0,0 +1,7 @@
+local Baseplate = Instance.new("Part")
+
+Baseplate.Anchored = true
+Baseplate.Parent = game.Workspace
+Baseplate.Size = Vector3.new(2048,2048,2048)
+Baseplate.Position = Vector3.new(0,0,0)
+Baseplate.Color = Color3.new(0.65098, 0.65098, 0.65098)
